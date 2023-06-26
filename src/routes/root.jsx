1394f3c7ba0956b9382e67b123ca1c806ed7a3cf
benjacoblee <benjacoblee@gmail.com>
2023-06-26 16:04:56 +0800

    :lipstick: Reuse styles from style file

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,24 +1,14 @@
-import { Box, Container, Card } from "@mui/material";
+import { Box, Container } from "@mui/material";
 import { Link } from "react-router-dom";
 import ReusableBtn from "../components/ReusableBtn";
+import { centeredBox } from "../components/styles";
 import "../global.css";
-import {
-  aggridCardContainer,
-  centeredBox,
-  locationOptionsBox
-} from "../components/styles";
 
 export default function Root() {
   return (
     <>
       <Container sx={{ height: "100vh" }}>
-        <Box
-          display="flex"
-          flexDirection="column"
-          justifyContent="center"
-          alignItems="center"
-          height="100%"
-        >
+        <Box sx={centeredBox}>
           <Box component="h1">Cafe Employees</Box>
           <Box sx={{ display: "flex" }}>
             <Box sx={{ mr: 2 }}>
